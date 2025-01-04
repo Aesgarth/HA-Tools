67811d88ee9e5fdb1c625b8061ce5ef414f5b00e
Aesgarth <getflair@gmail.com>
2025-01-04 12:33:47 +0000

    Update run.sh

--- grocy_itemscan/run.sh
+++ grocy_itemscan/run.sh
@@ -11,6 +11,9 @@ GROCY_URL=$(bashio::config 'grocy_url')
 bashio::log.info "Using API Key: ${API_KEY}"
 bashio::log.info "Grocy URL: ${GROCY_URL}"
 
+# Start the Flask application
+exec python3 /app/app.py
+
 # Start the service (replace this with your application logic)
 bashio::log.info "Starting HTTP server on port 5080..."
 python3 -m http.server 5080 
